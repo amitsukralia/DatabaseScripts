2e7c46fec467bcbbb5373f897754540d613d539b
AmitSukralia <32533264+amitsukralia@users.noreply.github.com>
2018-08-06 11:53:12 +1000

    updated to generic structure

--- Rebuild_Indexes.sql
+++ Rebuild_Indexes.sql
@@ -1,34 +1,8 @@
-ALTER INDEX ALL ON ODMS.dbo.HBDHWDBearingWheelDetail_Copy REBUILD;
-ALTER INDEX ALL ON ODMS.dbo.HBDHWDBearingWheelDetail REORGANIZE;
+ALTER INDEX ALL ON [TableName] REBUILD;
+ALTER INDEX ALL ON [TableName] REORGANIZE;
 
-create index IDX_NC_CaptureStartDateTime 
+drop index [TableName].[IndexName]
 
-drop index dbo.HBDHWDBearingWheelDetail.[IDX_NC_HBDHWD]
+create index [IndexName] on TableName(ColumnList)
+INCLUDE(ColumnList)
 
-create index [IDX_NC_ReportDate] on dbo.HBDHWDBearingWheelDetail(ReportDate, IsActive)
-INCLUDE([TrainId],[SourceSystemTrainId], [SiteName],[VehicleOwnerName], [VehicleRunningNumber], [DeviceName])
-
-
-ALTER INDEX  [IDX_NC_VehicleTravelSection] ON EventRecorder.FIRE REBUILD
-GO
-
-ALTER INDEX  [IDX_NC_GPSCoordinate] ON Location.GPSCoordinate REBUILD
-GO
-
-
-ALTER INDEX  [IDX_NC_TripRecordDateTime] ON EventRecorder.FIRE REBUILD
-GO
-
-ALTER INDEX  [IDX_NC_Vehicle] ON RollingStock.Vehiclde REBUILD
-GO
-
-ALTER INDEX  [PK_Vehicle] ON RollingStock.Vehiclde REBUILD
-GO
-
-ALTER INDEX  [IDX_NC_Axle] ON RollingStock.Axle REBUILD
-GO
-
-ALTER INDEX  [PK_Axle] ON RollingStock.Axle REBUILD
-GO
-
-ALTER INDEX  ALL ON Log.FileImportLog  REBUILD
